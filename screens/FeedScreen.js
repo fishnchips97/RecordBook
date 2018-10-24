0acf9170bc86eb9f096a3a1d96d56ff384f875b6
Erik Fisher <ebenfishy@gmail.com>
2018-10-24 07:35:32 -0700

    Began Feed screen
    Began Feed screen with filler data

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, FlatList, Image } from 'react-native';
 
 export default class FeedScreen extends React.Component {
     static navigationOptions = {
@@ -8,21 +8,53 @@ export default class FeedScreen extends React.Component {
 
     render() {
         return (
-            <ScrollView style={styles.container}>
-                {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-                <View>
-                    <Text>Feed</Text>
-                </View>
-            </ScrollView>
+            <View style={styles.container}>
+                <FlatList
+                    data={[
+                        { key: '1', name: 'Steve',  distance: 3.5, pace: 8.0},
+                        { key: '2', name: 'Jon', distance: 2.2, pace: 7.0},
+                        { key: '3', name: 'Ben', distance: 1.1, pace: 6.0},
+                    ]}
+                    renderItem = {_renderRunEntry}
+                />
+            </View>    
         );
     }
+}
+
+function _renderRunEntry({item}) {
+    itemWindow = 
+        <View style={styles.item}>
+            <View style = {{flexDirection: "row"}}>
+                <View>
+                <Image source={{ uri: "https://facebook.github.io/react-native/img/favicon.png", width: 20, height: 20 }} />
+                </View>
+                <View>
+                    <Text style={{paddingLeft: 10}}>{item.name}</Text>
+                </View>
+            </View>
+
+            <View style={{flexDirection: "row"}}>
+                <Text style={{paddingLeft: 30}}>Distance: {item.distance} miles</Text>
+                <Text style={{paddingLeft: 30}}>Pace: {item.pace} min/mile</Text>
+            </View>
+            
+            
+        </View>
+
+    return itemWindow;
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 15,
-        backgroundColor: '#fff',
+        paddingTop: 10,
+        backgroundColor: '#eee',
     },
+    item: {
+        padding: 10,
+        backgroundColor: '#fff',
+        marginBottom: 10,
+        flexDirection: 'column',
+    }
 });
